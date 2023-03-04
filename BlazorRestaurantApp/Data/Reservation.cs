82aee651b11af66dc4cb4191821493687ad48b93
riazonbin <riazonbin@mail.ru>
2023-03-04 10:56:14 +0300

    added fields for reservation and table classes

--- BlazorRestaurantApp/Data/Reservation.cs
+++ BlazorRestaurantApp/Data/Reservation.cs
@@ -1,6 +1,23 @@
-﻿namespace BlazorRestaurantApp.Data
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BlazorRestaurantApp.Data
 {
     public class Reservation
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string ReservedCustomerId { get; set; }
+
+        public DateTime TimeOfReservation { get; set; }
+
+        public DateTime EndTimeOfReservation { get; set; }
+
+        public Reservation()
+        {
+            EndTimeOfReservation = TimeOfReservation.AddHours(3);
+        }
     }
 }
--- BlazorRestaurantApp/Data/Table.cs
+++ BlazorRestaurantApp/Data/Table.cs
@@ -1,6 +1,15 @@
-﻿namespace BlazorRestaurantApp.Data
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BlazorRestaurantApp.Data
 {
     public class Table
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public int TableNumber { get; set; }
+
+        public Reservation TableReservation { get; set; }
     }
 }
