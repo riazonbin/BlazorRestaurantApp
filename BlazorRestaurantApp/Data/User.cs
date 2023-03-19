eaf69031add88114eac0a610fd355c11fcc887fa
riazonbin <riazonbin@mail.ru>
2023-03-19 13:59:00 +0300

    added new attributes for user data

--- BlazorRestaurantApp/Data/User.cs
+++ BlazorRestaurantApp/Data/User.cs
@@ -11,13 +11,20 @@ namespace BlazorRestaurantApp.Data
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
+
         [Required(ErrorMessage = "Фамилия должна быть заполнена!")]
+        [StringLength(30, ErrorMessage = "Минимальное кол-во символов в фамилии 4, максимальное - 30!", MinimumLength = 4)]
+        [RegularExpression("^[А-Яа-я]+$", ErrorMessage = "Фамилия не может содержать цифр или букв не на кириллице!")]
         public string Lastname { get; set; }
 
         [Required(ErrorMessage = "Имя должно быть заполнено!")]
+        [StringLength(30, ErrorMessage = "Минимальное кол-во символов в имени 2, максимальное - 30!", MinimumLength = 2)]
+        [RegularExpression("^[А-Яа-я]+$", ErrorMessage = "Имя не может содержать цифр или букв не на кириллице!")]
         public string Firstname { get; set; }
 
         [Required(ErrorMessage = "Отчество должно быть заполнено!")]
+        [StringLength(30, ErrorMessage = "Минимальное кол-во символов в отчестве 4, максимальное - 30!", MinimumLength = 4)]
+        [RegularExpression("^[А-Яа-я]+$", ErrorMessage = "Отчество не может содержать цифр или букв не на кириллице!")]
         public string Patronymic { get; set; }
 
         [Required(ErrorMessage = "Почта должна быть заполнена!")]
